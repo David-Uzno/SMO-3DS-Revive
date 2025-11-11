b1a5ab7635bdf52bc5b61419ea87925229dd7e75
David <141585041+David-Uzno@users.noreply.github.com>
2025-11-11 19:14:33 -0300

    refactor(scr_behaviorStake): renombrar variables para mejorar la legibilidad

--- Assets/Objects/Stake/scr_behaviorStake.cs
+++ Assets/Objects/Stake/scr_behaviorStake.cs
@@ -9,19 +9,19 @@ public class scr_behaviorStake : MonoBehaviour
     private readonly string _cappyMountPath = "Armature/AllRoot/Center/Stake/JointRoot/Bend0/Bend1/Bend2/CapPoint";
 
 	[Header("Bent")]
-	private float bentHorizontal;
-    private float bentVertical;
-    private float bentAmount;
-    private readonly float bentAmountMax = 20f;
+	private float _bentHorizontal;
+    private float _bentVertical;
+    private float _bentAmount;
+    private readonly float _bentAmountMax = 20f;
 
     [Header("Refences Bones")]
-    private Transform boneBend0;
-    private Transform boneBend1;
-    private Transform boneBend2;
-    private Transform cappyBone;
+    private Transform _boneBend0;
+    private Transform _boneBend1;
+    private Transform _boneBend2;
+    private Transform _cappyBone;
 
 	[Header("Animation")]
-	private Animator stakeAnimator;
+	private Animator _stakeAnimator;
 
     private void Start()
     {
@@ -40,29 +40,29 @@ public class scr_behaviorStake : MonoBehaviour
 
 	private void InitializeComponents()
 	{
-        stakeAnimator = GetComponent<Animator>();
-        boneBend0 = transform.Find("Armature/AllRoot/Center/Stake/JointRoot/Bend0");
-        boneBend1 = boneBend0.GetChild(0);
-        boneBend2 = boneBend1.GetChild(0);
-        cappyBone = transform.Find(_cappyMountPath);
+        _stakeAnimator = GetComponent<Animator>();
+        _boneBend0 = transform.Find("Armature/AllRoot/Center/Stake/JointRoot/Bend0");
+        _boneBend1 = _boneBend0.GetChild(0);
+        _boneBend2 = _boneBend1.GetChild(0);
+        _cappyBone = transform.Find(_cappyMountPath);
 	}
 
 
 	private void HandleBentAmount()
 	{
-		if (bentAmount < bentAmountMax && bentAmount != -0.1f)
+		if (_bentAmount < _bentAmountMax && _bentAmount != -0.1f)
 		{
-			bentAmount += 0.5f;
+			_bentAmount += 0.5f;
 		}
-		else if (bentAmount != -0.1f)
+		else if (_bentAmount != -0.1f)
 		{
 			// Stake extraído completamente
 			GetComponent<Collider>().enabled = false;
-			stakeAnimator.Play("pullOut");
+			_stakeAnimator.Play("pullOut");
 			MarioController.s.cappy.SetState(eStateCap.UnHack);
-			bentAmount = -0.1f;
+			_bentAmount = -0.1f;
 		}
-		else if (stakeAnimator.GetBool("isDead"))
+		else if (_stakeAnimator.GetBool("isDead"))
 		{
 			Destroy(gameObject);
 		}
@@ -74,16 +74,16 @@ public class scr_behaviorStake : MonoBehaviour
 
 	private void ApplyBoneBending()
 	{
-		Vector3 bendEuler = new Vector3(0, bentVertical * bentAmount, bentHorizontal * bentAmount);
-		boneBend0.localEulerAngles = bendEuler;
-		boneBend1.localEulerAngles = bendEuler;
-		boneBend2.localEulerAngles = bendEuler;
+		Vector3 bendEuler = new Vector3(0, _bentVertical * _bentAmount, _bentHorizontal * _bentAmount);
+		_boneBend0.localEulerAngles = bendEuler;
+		_boneBend1.localEulerAngles = bendEuler;
+		_boneBend2.localEulerAngles = bendEuler;
 	}
 
 	private void AdjustCappyOrientation()
 	{
-		cappyBone.localRotation = Quaternion.LookRotation(Vector3.up, cappyBone.up);
-		cappyBone.localEulerAngles = new Vector3(-cappyBone.localEulerAngles.y, 0, 90);
+		_cappyBone.localRotation = Quaternion.LookRotation(Vector3.up, _cappyBone.up);
+		_cappyBone.localEulerAngles = new Vector3(-_cappyBone.localEulerAngles.y, 0, 90);
 	}
 	
     public void OnCappyTrigger()
@@ -94,9 +94,9 @@ public class scr_behaviorStake : MonoBehaviour
     public void OnCappyHacked()
     {
         MarioController.s.cappy.SetTransformOffset(2, Vector3.zero, Vector3.zero);
-        stakeAnimator.Play("pull");
-        bentHorizontal = scr_manageInput.AxisDir(-1).x;
-        bentVertical = scr_manageInput.AxisDir(-1).y;
+        _stakeAnimator.Play("pull");
+        _bentHorizontal = scr_manageInput.AxisDir(-1).x;
+        _bentVertical = scr_manageInput.AxisDir(-1).y;
         this.enabled = true;
     }
 }
